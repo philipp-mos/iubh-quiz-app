568aaf9f04bedc2032f5b531deaf0f6569070098
Philipp Moser <info@philipp-moser.de>
2021-05-12 22:09:12 +0200

    Add separate Config-File for accessing / setting configs

--- web-app/src/__init__.py
+++ web-app/src/__init__.py
@@ -4,9 +4,12 @@ def create_app():
 
     app = Flask(
         __name__,
+        instance_relative_config=False,
         template_folder = "views",
         static_folder = "static"
     )
+
+    app.config.from_object('config.Config')
 
     with app.app_context():
         from . import routes
