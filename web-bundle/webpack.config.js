147ee6597822a813d24b67928e510741ca60365f
Philipp Moser <info@philipp-moser.de>
2021-05-12 21:46:11 +0200

    Setup first running bundle-routine

---
+++ web-bundle/webpack.config.js
@@ -0,0 +1,51 @@
+const path =  require('path');
+const extract = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        main: './src/scripts.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test:/\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: extract.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new extract({
+            filename: '[name].bundle.css'
+        })
+    ],
+
+    mode: 'development'
+}
