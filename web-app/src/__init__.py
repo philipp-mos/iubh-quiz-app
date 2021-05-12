2654c76b61f27298701813cf423f13ca2c7b0ad4
Philipp Moser <info@philipp-moser.de>
2021-05-12 22:34:36 +0200

    Add Connection to PostgreSQL Database

--- web-app/src/__init__.py
+++ web-app/src/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
+
+
 def create_app():
 
     app = Flask(
@@ -11,6 +15,9 @@ def create_app():
     )
 
     app.config.from_object('config.DevConfig')
+
+    db.init_app(app)
+
 
     with app.app_context():
         from .modules.home import home
