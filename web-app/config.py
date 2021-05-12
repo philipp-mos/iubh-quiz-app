1b6a9e5d802cf276490e5c771be22fcb2689b13a
Philipp Moser <info@philipp-moser.de>
2021-05-12 22:31:55 +0200

    Add SQLAlchemy Package and add Configuration Variables

--- web-app/config.py
+++ web-app/config.py
@@ -11,6 +11,11 @@ class Config:
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
 
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 
 
 class ProdConfig(Config):
