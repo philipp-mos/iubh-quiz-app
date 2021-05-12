706922431a5dcfa170139321cb0af3ba76c0f3cd
Philipp Moser <info@philipp-moser.de>
2021-05-12 23:00:09 +0200

    Add bashscript to automatically setup devenv

---
+++ setupDevEnv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+secret_key=
+sqlalchemy_database_uri=
+
+
+cd web-app
+
+if [ -d "env" ]
+then
+    source env/bin/activate
+else
+    python3 -m venv env
+    source env/bin/activate
+fi
+
+export SECRET_KEY=$secret_key
+export SQLALCHEMY_DATABASE_URI=$sqlalchemy_database_uri
+
+export LDFLAGS=-L/usr/local/opt/openssl/lib
+export CPPFLAGS=-I/usr/local/opt/openssl/include
+
+pip install -r requirements.txt
+
+
+
+cd ../web-bundle
+npm install
+npm run build
