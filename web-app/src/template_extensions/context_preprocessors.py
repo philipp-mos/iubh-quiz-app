58bca8dd7d6d14caa8284fc9be91233a02d131af
Philipp Moser <info@philipp-moser.de>
2021-05-17 22:45:40 +0200

    Surround file-access with try/catch

--- web-app/src/template_extensions/context_preprocessors.py
+++ web-app/src/template_extensions/context_preprocessors.py
@@ -4,7 +4,12 @@ from flask import current_app as app
 @app.context_processor
 def inject_bundle_version():
 
-    with app.open_resource('static/bundle-version.txt', 'r') as version_file:
-        version_number = version_file.read()
+    version_number = ''
+
+    try:
+        with app.open_resource('static/bundle-version.txt', 'r') as version_file:
+            version_number = version_file.read()
+    except FileNotFoundError:
+        print('bundle-version.txt does not exist')
 
     return dict(bundle_version=version_number)
