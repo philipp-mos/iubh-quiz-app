b9af7bd8d4f2669f33b9a7d4a0b26bed6adac42e
Philipp Moser <info@philipp-moser.de>
2021-05-19 17:48:53 +0200

    IQA-91: Update PostgreSQL Docker Configuration

--- web-app/config.py
+++ web-app/config.py
@@ -1,18 +1,15 @@
-from os import environ, path
-from dotenv import load_dotenv
+import os
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, "antenv"))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     FLASK_APP = 'app.py'
 
-    SECRET_KEY = environ.get("SECRET_KEY")
-    FLASK_APP = environ.get("FLASK_APP")
-    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    FLASK_ENV = os.getenv("FLASK_ENV", "development")
 
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
