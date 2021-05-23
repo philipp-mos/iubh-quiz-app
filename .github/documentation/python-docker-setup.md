93875043a190d7fa2ed53a6eb3f298c7cdb7a7ff
Philipp Moser <info@philipp-moser.de>
2021-05-23 11:00:44 +0200

    Update Configuration-Handling with .env File

--- .github/documentation/python-docker-setup.md
+++ .github/documentation/python-docker-setup.md
@@ -2,17 +2,16 @@
 Install Docker-Desktop https://www.docker.com/products/docker-desktop
 
 
-## Adjust the Configuration Source Class
-Update the Configuration-Source-Class from "config.ConfigLocal" to "config.ConfigDocker" in web-app/src/__init__.py
+## Add your .env Configuration
+Create your .env Configuration based on the .env.example File
 
 ## Start
 ```
 docker-compose up
 ```
-Default binding: 127.0.0.1:5000
 
 ## Stop
-This shuts down / remove the Containers and remove the network
+This shuts down / removes the Containers and remove the network
 ```
 docker-compose down
 ```
--- web-app/config.py
+++ web-app/config.py
@@ -4,13 +4,19 @@ from dotenv import load_dotenv
 
 class Config(object):
 
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    load_dotenv(os.path.join(basedir, '.env'))
+
     FLASK_APP = 'app.py'
 
-    FLASK_ENV = 'development'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    FLASK_ENV = os.environ.get("FLASK_ENV")
 
     DEBUG = False
     TESTING = False
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -21,28 +27,3 @@ class Config(object):
         SQLALCHEMY_ECHO = True
     elif FLASK_ENV == 'staging' :
         TESTING = True
-
-
-
-class ConfigLocal(Config):
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    load_dotenv(os.path.join(basedir, "antenv"))
-
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-
-    FLASK_ENV = os.environ.get("FLASK_ENV", "development")
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-
-
-
-
-class ConfigDocker(Config):
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    FLASK_ENV = os.getenv("FLASK_ENV", "development")
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
