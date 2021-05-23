ba417cca8b2052d5e4524d603400de85fe07e56d
Philipp Moser <info@philipp-moser.de>
2021-05-23 18:45:30 +0200

    IQA-95: Setup Api Error Results

--- web-app/src/template_extensions/error_handlers.py
+++ web-app/src/template_extensions/error_handlers.py
@@ -1,18 +1,32 @@
+"""
+Web Error-Handling
+"""
 from flask import render_template
 from flask import current_app as app
 
-
-def not_found(e):
-    """Returns 404 Not Found Errorpage"""
+def web_not_found(e):
+    """Returns Web-404 Not Found Errorpage"""
     return render_template('errorpages/404.html'), 404
 
 
-def server_error(e):
-    """Returns 500 Server Error Errorpage"""
+def web_server_error(e):
+    """Returns Web-500 Server Error Errorpage"""
     return render_template('errorpages/500.html'), 500
 
 
+app.register_error_handler(404, web_not_found)
+app.register_error_handler(500, web_server_error)
 
 
-app.register_error_handler(404, not_found)
-app.register_error_handler(500, server_error)
+
+
+"""
+API Error-Handling
+"""
+from flask import jsonify
+from ..api.v1.subjects import api_v1__subjects_controller
+
+@api_v1__subjects_controller.errorhandler(500)
+def api_server_error(e):
+    """Returns Api-500 Server Error"""
+    return jsonify(error=str(e)), 500
