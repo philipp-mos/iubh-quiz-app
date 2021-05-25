509a36071d13a3e54e49e2361fcfdb82309e1a5a
Philipp Moser <info@philipp-moser.de>
2021-05-25 23:04:21 +0200

    IQA-98: Reset using Repository-Baseclass for generic requests

--- web-app/src/repositories/Repository.py
+++ web-app/src/repositories/Repository.py
@@ -1,11 +1,10 @@
 from .abstracts.AbcRepository import AbcRepository
-from ..models.Subject import Subject
 
 # TODO: Switch to usage of Generics
 class Repository(AbcRepository):
 
     def get_all():
-        return Subject.query.all()
+        return NotImplementedError
 
     def find_by_id(id):
-        return Subject.query.get(id)
+        return NotImplementedError
