b13a253d8666f4581d34ea6c753af151d0e1e256
Philipp Moser <info@philipp-moser.de>
2021-05-29 16:11:35 +0200

    IQA-56: Add new UserRole Model and create Migration

---
+++ web-app/src/models/UserRole.py
@@ -0,0 +1,17 @@
+from .. import db
+
+class UserRole(db.Model):
+
+    __tablename__ = 'userroles'
+
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
