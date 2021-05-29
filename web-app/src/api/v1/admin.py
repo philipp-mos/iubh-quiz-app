267dcd51b7241630b3b3ae8ae3cccbe36ebb1978
Philipp Moser <info@philipp-moser.de>
2021-05-29 23:29:41 +0200

    IQA-111: Add new Api Admin Route to get current App-Version

--- web-app/src/api/v1/admin.py
+++ web-app/src/api/v1/admin.py
@@ -1,7 +1,7 @@
 from flask import current_app as app
 from flask import Blueprint, jsonify, request
 from flask_migrate import upgrade
-
+from os import path
 
 
 api_v1__admin_controller = Blueprint(
@@ -20,3 +20,19 @@ def run_migrations():
         return jsonify({ 'status': 'success' }), 200
 
     return jsonify({ 'status': 'denied' }), 403
+
+
+@api_v1__admin_controller.route('/app-version', methods=['GET'])
+def get_appversion():
+
+    version_number = '0.0.0'
+
+    version_file_path = path.join(app.root_path, '../version.txt')
+
+    try:
+        with app.open_resource(version_file_path, 'r') as version_file:
+            version_number = version_file.read()
+    except FileNotFoundError:
+        print(version_file_path + ' does not exist')
+
+    return jsonify({ 'version': version_number })
