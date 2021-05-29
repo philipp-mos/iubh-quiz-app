bed8cc1c6f8ce2c0aed650f689a7fed349734f11
Philipp Moser <info@philipp-moser.de>
2021-05-29 10:26:08 +0200

    IQA-100: Check if user is authenticated and redirect to Dashboard

--- web-app/src/modules/auth/auth.py
+++ web-app/src/modules/auth/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from flask_login import current_user
 
 from .viewmodels.LoginViewModel import LoginViewModel
 
@@ -14,6 +15,9 @@ auth_controller = Blueprint(
 ## Auth/Login ##
 @auth_controller.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('home_controller.index'))
 
     return render_template(
         'login.jinja2',
