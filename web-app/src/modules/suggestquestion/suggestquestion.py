5c55e19eb306c7c63c5ead9539f768760b94942a
Philipp Moser <info@philipp-moser.de>
2021-06-03 17:47:53 +0200

    IQA-22: Setup suggestquestion_controller

---
+++ web-app/src/modules/suggestquestion/suggestquestion.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+from flask import render_template
+from flask_login import login_required
+
+
+suggestquestion_controller = Blueprint(
+    'suggestquestion_controller',
+    __name__,
+    template_folder='views',
+    url_prefix='/suggest-question'
+)
+
+
+@suggestquestion_controller.before_request
+@login_required
+def before_request():
+    pass
+
+
+
+## QuestionSuggest/Index ##
+@suggestquestion_controller.route('/overview', methods=['GET'])
+def index():
+    """
+    Question-Suggest First Page
+    """
+
+    return render_template('suggestquestion_index.jinja2')
