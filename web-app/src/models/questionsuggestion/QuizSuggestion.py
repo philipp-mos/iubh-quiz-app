679f84cfdc4a8601e633e0eafbf32853f6378cee
Philipp Moser <info@philipp-moser.de>
2021-06-05 11:20:38 +0200

    IQA-25: Add QuizSuggestion-Model

---
+++ web-app/src/models/questionsuggestion/QuizSuggestion.py
@@ -0,0 +1,33 @@
+from ... import db
+
+class QuizSuggestion(db.Model):
+
+    __tablename__ = 'quiz_suggestions'
+
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    question = db.Column(
+        db.String(),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+
+    creation_date = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+
+    is_approved = db.Column(
+        db.Boolean()
+    )
+
+    is_declined = db.Column(
+        db.Boolean()
+    )
