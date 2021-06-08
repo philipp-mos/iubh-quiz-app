98f5c302b97dec475772090e2da6ec30f515bd31
MrShaxx <84182865+MrShaxx@users.noreply.github.com>
2021-06-08 13:32:40 +0200

    Update subjects.py

--- web-app/src/modules/subjects/subjects.py
+++ web-app/src/modules/subjects/subjects.py
@@ -2,13 +2,6 @@ from flask import Blueprint
 from flask import render_template
 from flask_login import login_required
 
-home_controller = Blueprint(
-    'home_controller',
-    __name__,
-    template_folder='views',
-    url_prefix=''
-)
-
 subjects_controller = Blueprint(
     'subjects_controller',
     __name__,
@@ -17,7 +10,7 @@ subjects_controller = Blueprint(
 )
 
 
-@home_controller.before_request
+@subjects_controller.before_request
 @login_required
 def before_request():
     pass
