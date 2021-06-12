e91f0831cb82d21184f2c1ddbb48165e22657a11
Philipp Moser <info@philipp-moser.de>
2021-06-12 12:17:15 +0200

    IQA-122: Implement dynamic values based on currently logged in user

--- web-app/src/modules/user/user.py
+++ web-app/src/modules/user/user.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+
+from ...repositories.UserRepository import UserRepository
+
+from ...services.UserService import UserService
+
+from .viewmodels.UserProfileViewModel import UserProfileViewModel
 
 
 user_controller = Blueprint(
@@ -24,4 +30,21 @@ def profile():
     User Profile Overview Page
     """
 
-    return render_template('profile.jinja2')
+    user = UserRepository().find_by_id(current_user.id)
+    
+    user_status = '-'
+
+    if UserService.is_user_tutor(user):
+        user_status = 'Tutor'
+    elif UserService.is_user_student(user):
+        user_status = 'Student'
+
+
+    return render_template(
+        'profile.jinja2',
+        viewmodel=UserProfileViewModel(
+            user.email,
+            user.creation_date.strftime("%d.%m.%Y"),
+            user_status
+        )
+    )
---
+++ web-app/src/modules/user/viewmodels/UserProfileViewModel.py
@@ -0,0 +1,8 @@
+
+class UserProfileViewModel():
+
+    def __init__(self, email, registered_since, status):
+        
+        self.email = email
+        self.registered_since = registered_since
+        self.status = status
