a66772b947817e06aee37086e00279fe2967dac3
Philipp Moser <info@philipp-moser.de>
2021-06-20 10:17:45 +0200

    IQA-125: Implement get_from_key CacheManager Method

--- web-app/src/managers/CacheManager.py
+++ web-app/src/managers/CacheManager.py
@@ -1,2 +1,23 @@
-class CacheManager():
-    pass
+import time
+
+
+class CacheManager(object):
+
+    _cache_ = {}
+    EXPIRES = 0
+    VALUE = 1
+
+    @classmethod
+    def get_from_key(self, key: str):
+        """
+        Returns Value from Cache by Key
+        Deletes Key if Cached Value is expired
+        """
+        try:
+            if self._cache_[key][self.EXPIRES] > time.time():
+                return self._cache_[key][self.VALUE]
+            else:
+                del self._cache_[key]
+                return None
+        except KeyError:
+            return None
