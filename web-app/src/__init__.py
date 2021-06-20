0776f4c1651348bb9eae9efd4b138d354d8ea8fe
Philipp Moser <info@philipp-moser.de>
2021-06-20 10:33:43 +0200

    IQA-125: Implement set_by_key CachingManager Method

--- web-app/src/__init__.py
+++ web-app/src/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-from managers.CacheManager import CacheManager
+from .managers.CacheManager import CacheManager
 
 
 db = SQLAlchemy()
--- web-app/src/managers/CacheManager.py
+++ web-app/src/managers/CacheManager.py
@@ -1,5 +1,7 @@
 import time
 
+from flask import current_app as app
+
 
 class CacheManager(object):
 
@@ -21,3 +23,18 @@ class CacheManager(object):
                 return None
         except KeyError:
             return None
+
+    @classmethod
+    def set_by_key(self, key: str, value, duration=3600):
+        """
+        Add Value to Cache by CachingKey
+        """
+        try:
+            expire_time = time.time() + duration
+        except TypeError:
+            app.logger.error('')
+            raise TypeError("Dureation should be a numeric value")
+
+        self._cache_[key] = (value, expire_time)
+
+        return self.get_from_key(key)
