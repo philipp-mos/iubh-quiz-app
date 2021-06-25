ffcd599c4b852d0b2e53efb42307cbff7b2048ab
Philipp Moser <info@philipp-moser.de>
2021-06-25 21:39:04 +0200

    IQA-120: Register Model and create Migration

--- web-app/src/models/model_registration.py
+++ web-app/src/models/model_registration.py
@@ -1,4 +1,4 @@
 from .Subject import Subject  # noqa: F401
 from .user import User, UserRole, UserUserRole  # noqa: F401
 from .suggestquestion import QuizSuggestion, QuizSuggestionAnswer  # noqa: F401
-from .quiz import QuizQuestion  # noqa: F401
+from .quiz import QuizQuestion, QuizAnswer  # noqa: F401
