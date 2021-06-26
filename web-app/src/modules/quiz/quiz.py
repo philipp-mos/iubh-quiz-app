d75fe024685a9fe286d3d9c62ce11da94f37cc0c
Philipp Moser <info@philipp-moser.de>
2021-06-26 19:32:48 +0200

    IQA-119: Setup quiz_controller and question view

---
+++ web-app/src/modules/quiz/quiz.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template
+from flask_login import login_required
+
+
+quiz_controller = Blueprint(
+    'quiz_controller',
+    __name__,
+    template_folder='views',
+    url_prefix='/quiz'
+)
+
+
+@quiz_controller.before_request
+@login_required
+def before_request():
+    pass
+
+
+# Quiz/Question_1
+@quiz_controller.route('/question-1', methods=['GET'])
+def question_1():
+    """
+    Quiz Question 1
+    """
+
+    return render_template('question.jinja2')
