d8abda55f2f4b5a26c409941b855a7eb0cd376d6
Philipp Moser <info@philipp-moser.de>
2021-06-26 23:38:40 +0200

    IQA-119: Add Default Case for Route without Parameter and update ViewModel

--- web-app/src/modules/quiz/quiz.py
+++ web-app/src/modules/quiz/quiz.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required
 
 from .viewmodels.QuizQuestionViewModel import QuizQuestionViewModel
@@ -18,24 +18,35 @@ def before_request():
     pass
 
 
+# Quiz/Question
+@quiz_controller.route('/question', methods=['GET'])
+def question_fallback():
+    """
+    Fallback Route for Question-Action without Parameter
+    """
+    return redirect(url_for('quiz_controller.question', question_number=1))
+
+
 # Quiz/Question/{question_number}
 @quiz_controller.route('/question/<int:question_number>', methods=['GET', 'POST'])
 def question(question_number: int):
     """
     Quiz Question
     """
-
     viewmodel = QuizQuestionViewModel()
 
+    if request.method == 'POST' and viewmodel.validate_on_submit():
+        return redirect(url_for('quiz_controller.question', question_number=question_number + 1))
+
     viewmodel.question_text = 'Lorem ipsum dolor sit amet?'
+
+    viewmodel.question_number = question_number
 
     viewmodel.answers = {
         'A': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         'B': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         'C': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     }
-
-    viewmodel.question_number = question_number
 
     return render_template(
         'question.jinja2',
--- web-app/src/modules/quiz/viewmodels/QuizQuestionViewModel.py
+++ web-app/src/modules/quiz/viewmodels/QuizQuestionViewModel.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, SubmitField, RadioField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, RadioField
 
 
 class QuizQuestionViewModel(FlaskForm):
@@ -9,12 +8,7 @@ class QuizQuestionViewModel(FlaskForm):
 
     answers = {}
 
-    question_number = HiddenField(
-        'question-number',
-        validators=[
-            DataRequired()
-        ]
-    )
+    question_number: int = 0
 
     answer_selection = RadioField(choices=[('A', 'a'), ('B', 'b'), ('C', 'c')])
 
