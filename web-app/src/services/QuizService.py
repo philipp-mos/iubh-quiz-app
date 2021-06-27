eeaa4b15624bee68b2b4f781b972167af5d3346b
Philipp Moser <info@philipp-moser.de>
2021-06-27 21:14:33 +0200

    IQA-85: Start implementing InitQuizGame Method

--- web-app/src/services/QuizService.py
+++ web-app/src/services/QuizService.py
@@ -1,10 +1,20 @@
+from flask_login import current_user
+from datetime import datetime
+
 from .abstracts.AbcQuizService import AbcQuizService
 
 from ..models.quizgame.QuizGame import QuizGame
+from ..models.quizgame.QuizGameStatus import QuizGameStatus
 
 
 class QuizService(AbcQuizService):
 
     @staticmethod
     def initialize_quiz_game_for_subject(subject_id: int) -> QuizGame:
-        pass
+        quiz_game = QuizGame()
+        quiz_game.creation_date = datetime.now()
+        quiz_game.current_assignee_id = current_user.get_id()
+        quiz_game.current_status = QuizGameStatus.IN_PROGRESS
+        quiz_game.subject_id = subject_id
+
+        return quiz_game
