4a32ec514644e2c28c884eea417cef600dd3c102
Philipp Moser <info@philipp-moser.de>
2021-07-05 16:54:08 +0200

    IQA-85: Add first_test to make tests run again

---
+++ web-app/tests/first_test.py
@@ -0,0 +1,12 @@
+def addition(a, b):
+    return a + b
+
+
+def test_first_test():
+    """
+    Test addition Function-Result
+    """
+
+    print("\nRunning first Test")
+
+    assert addition(5, 2) == 7
