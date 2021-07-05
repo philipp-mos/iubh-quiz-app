c95703e4cbf5c25d09489950784e8f25d6a4064d
Philipp Moser <info@philipp-moser.de>
2021-07-05 15:45:35 +0200

    IQA-85: Remove SampleTest and apply Flake8 Rules to Tests

--- web-app/tests/services/test_notificationservice.py
+++ web-app/tests/services/test_notificationservice.py
@@ -4,5 +4,5 @@ from src.services.NotificationService import NotificationService
 
 
 def test_notificationservice_send_notification():
-    with pytest.raises(NotImplementedError) as exception:
+    with pytest.raises(NotImplementedError) as exception:  # noqa: F841
         NotificationService.send_notification('email@test.com', 'Email-Subject', 'Sample Content')
--- web-app/tests/services/test_subjectservice.py
+++ web-app/tests/services/test_subjectservice.py
@@ -4,13 +4,14 @@ from src.services.SubjectService import SubjectService
 from src.models.subject.Subject import Subject
 from src.api.v1.dtos.SubjectDto import SubjectDto
 
+
 def test_subjectservice_subjectlist_to_subjectdtolist_mapping():
 
     list_of_subjects = [
-         Subject('Mathematik I'),
-         Subject('Mathematik II'),
-         Subject('Grundlagen der industriellen Softwaretechnik'),
-         Subject('Financial Services Management')
+        Subject('Mathematik I'),
+        Subject('Mathematik II'),
+        Subject('Grundlagen der industriellen Softwaretechnik'),
+        Subject('Financial Services Management')
     ]
 
     list_of_subjectdtos = [
@@ -22,22 +23,16 @@ def test_subjectservice_subjectlist_to_subjectdtolist_mapping():
 
     result = SubjectService.subjectlist_to_subjectdtolist_mapping(list_of_subjects)
 
-
     assert is_equal_list_of_subjectdtos(list_of_subjectdtos, result)
-
-
-
 
 
 def is_equal_list_of_subjectdtos(first_list: List[SubjectDto], second_list: List[SubjectDto]) -> bool:
 
     if len(first_list) != len(second_list):
         return False
-    
 
     for i in range(0, len(first_list)):
         if first_list[i].name != second_list[i].name:
             return False
-
 
     return True
