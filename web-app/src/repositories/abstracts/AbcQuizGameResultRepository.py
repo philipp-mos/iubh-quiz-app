04b26cfefd856136637e0fa36d5a4a0f3a6dd6b3
Philipp Moser <info@philipp-moser.de>
2021-07-25 10:34:37 +0200

    IQA-135: Remove whitespaces to fix Flask8 Issue

--- web-app/src/repositories/abstracts/AbcQuizGameResultRepository.py
+++ web-app/src/repositories/abstracts/AbcQuizGameResultRepository.py
@@ -3,7 +3,7 @@ from .AbcRepository import AbcRepository
 
 
 class AbcQuizGameResultRepository(AbcRepository):
-    
+
     @abstractmethod
     def count_by_user_id(user_id: int) -> int:
         raise NotImplementedError
