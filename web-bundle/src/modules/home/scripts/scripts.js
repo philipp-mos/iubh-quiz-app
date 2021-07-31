645f3c6469e815bf8078e1bfbf3d6f5888e83c2d
Philipp Moser <info@philipp-moser.de>
2021-07-31 11:42:40 +0200

    IQA-140: Setup Chart Initialization

--- web-bundle/src/modules/home/scripts/scripts.js
+++ web-bundle/src/modules/home/scripts/scripts.js
@@ -1,3 +1,32 @@
 import Chart from 'chart.js/auto';
 
 import '../styles/styles.scss';
+
+const chartCanvas = document.querySelector('#dashboardHistoryChart');
+
+var resultChart = new Chart(chartCanvas, {
+    type: 'bar',
+    data: {
+        labels: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+        datasets: [
+            {
+                label: 'Gewonnene Spiele',
+                data: [],
+                backgroundColor: '#75b798',
+            },
+            {
+                label: 'Verlorene Spiele',
+                data: [],
+                backgroundColor: '#ea868f',
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            }
+        }
+    }
+});
